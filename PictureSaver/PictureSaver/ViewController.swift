7bbf6573ab95c588faad024a56e4af14728bd545
Artsiom Stulba <lizoksame@MacBook-Air13-M1.local>
2025-12-20 21:35:46 +0300

    Add project

---
+++ PictureSaver/PictureSaver/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureSaver
+//
+//  Created by LizOk&Same on 20.12.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
